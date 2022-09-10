025e2a70be764400382f0decb59436ae313c203a
sklakshmana <sklakshmana@gmail.com>
2022-09-10 14:33:37 +0530

    New Project Added

---
+++ GitDemoJava/src/com/nxtgenai/demo/WelcomeUser.java
@@ -0,0 +1,11 @@
+package com.nxtgenai.demo;
+
+public class WelcomeUser {
+
+	public static void main(String[] args) {
+		// Code added by Lakshman
+		System.out.println("Welcome to Github Session");
+
+	}
+
+}
